1a79a2628f9527f9e6d91321e48e9493bd43cda9
Kyle Ross <kylejamesross@gmail.com>
2022-08-17 22:38:56 -0600

    Add null-ls for formating and linting

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -74,6 +74,7 @@ return packer.startup(function(use)
     use "neovim/nvim-lspconfig" -- enable LSP
     use "williamboman/nvim-lsp-installer" -- simple to use language server installer
     use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+    use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
     -- snippets
     use "L3MON4D3/LuaSnip" --snippet engine
