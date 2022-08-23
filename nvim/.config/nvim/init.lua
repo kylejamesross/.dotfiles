34d7aa5214c677c7124e38b7301f8e4c12d98471
Kyle Ross <kylejamesross@gmail.com>
2022-08-22 20:43:51 -0600

    Install lspsaga

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,21 +1,22 @@
-require "user.impatient"
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.telescope-config"
-require "user.treesitter"
-require "user.colorizer"
-require "user.autopairs"
-require "user.comment"
-require "user.gitsigns"
-require "user.nvim-tree"
-require "user.bufferline"
-require "user.lualine"
-require "user.toggleterm"
-require "user.indent-blankline"
-require "user.illuminate"
-require "user.alpha"
+require("user.impatient")
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.colorscheme")
+require("user.cmp")
+require("user.lsp")
+require("user.telescope")
+require("user.telescope-config")
+require("user.treesitter")
+require("user.colorizer")
+require("user.autopairs")
+require("user.comment")
+require("user.gitsigns")
+require("user.nvim-tree")
+require("user.bufferline")
+require("user.lualine")
+require("user.toggleterm")
+require("user.indent-blankline")
+require("user.illuminate")
+require("user.alpha")
+require("user.lspsaga")
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,127 +1,126 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
-]]
+]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
+	-- My plugins here
+	use("wbthomason/packer.nvim") -- Have packer manage itself
 
-  if vim.g.vscode then
-    use { 'svetliakov/vim-easymotion', as = 'vsc-easymotion' }
-  else
-    -- Regular nvim plugins
-    use { 'easymotion/vim-easymotion' }
-    use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-    use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-    use "windwp/nvim-autopairs"
-    use "numToStr/Comment.nvim" -- Easily comment stuff
-    use "akinsho/toggleterm.nvim"
-    use 'lewis6991/impatient.nvim'
-    use "lukas-reineke/indent-blankline.nvim"
-    use {
-        'goolord/alpha-nvim',
-        requires = { 'kyazdani42/nvim-web-devicons' },
-    }
-    -- tree
-    use {
-      'kyazdani42/nvim-tree.lua',
-      requires = {
-        'kyazdani42/nvim-web-devicons', -- optional, for file icons
-      },
-    }
+	if vim.g.vscode then
+		use({ "svetliakov/vim-easymotion", as = "vsc-easymotion" })
+	else
+		-- Regular nvim plugins
+		use({ "easymotion/vim-easymotion" })
+		use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+		use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
+		use("windwp/nvim-autopairs")
+		use("numToStr/Comment.nvim") -- Easily comment stuff
+		use("akinsho/toggleterm.nvim")
+		use("lewis6991/impatient.nvim")
+		use("lukas-reineke/indent-blankline.nvim")
+		use({
+			"goolord/alpha-nvim",
+			requires = { "kyazdani42/nvim-web-devicons" },
+		})
+		-- tree
+		use({
+			"kyazdani42/nvim-tree.lua",
+			requires = {
+				"kyazdani42/nvim-web-devicons", -- optional, for file icons
+			},
+		})
 
+		-- cmp plugins
+		use("hrsh7th/nvim-cmp") -- the completion plugin
+		use("hrsh7th/cmp-buffer") -- buffer completions
+		use("hrsh7th/cmp-path") -- path completions
+		use("hrsh7th/cmp-cmdline") -- cmdline completions
+		use("saadparwaiz1/cmp_luasnip") -- snippet completions
+		use("hrsh7th/cmp-nvim-lsp")
+		use("hrsh7th/cmp-nvim-lua")
 
-    -- cmp plugins
-    use "hrsh7th/nvim-cmp" -- the completion plugin
-    use "hrsh7th/cmp-buffer" -- buffer completions
-    use "hrsh7th/cmp-path" -- path completions
-    use "hrsh7th/cmp-cmdline" -- cmdline completions
-    use "saadparwaiz1/cmp_luasnip" -- snippet completions
-    use "hrsh7th/cmp-nvim-lsp"
-    use "hrsh7th/cmp-nvim-lua"
+		-- LSP
+		use("neovim/nvim-lspconfig") -- enable LSP
+		use("williamboman/nvim-lsp-installer") -- simple to use language server installer
+		use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for
+		use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
+		--[[ use "jose-elias-alvarez/nvim-lsp-ts-utils" ]]
+		use({ "RRethy/vim-illuminate" })
+		use({ "glepnir/lspsaga.nvim" })
 
-     -- LSP
-    use "neovim/nvim-lspconfig" -- enable LSP
-    use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-    use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
-    use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-    --[[ use "jose-elias-alvarez/nvim-lsp-ts-utils" ]]
-    use { "RRethy/vim-illuminate" }
+		-- snippets
+		use("L3MON4D3/LuaSnip") --snippet engine
+		use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
-    -- snippets
-    use "L3MON4D3/LuaSnip" --snippet engine
-    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+		-- treesitter
+		use({
+			"nvim-treesitter/nvim-treesitter",
+			run = ":TSUpdate",
+		})
+		use("JoosepAlviste/nvim-ts-context-commentstring")
+		use("p00f/nvim-ts-rainbow")
 
-    -- treesitter
-    use {
-      "nvim-treesitter/nvim-treesitter",
-      run = ":TSUpdate",
-    }
-    use 'JoosepAlviste/nvim-ts-context-commentstring'
-    use 'p00f/nvim-ts-rainbow'
+		--colors
+		use("NvChad/nvim-colorizer.lua")
 
-    --colors
-    use 'NvChad/nvim-colorizer.lua'
+		-- Telescope
+		use("nvim-telescope/telescope.nvim")
+		use("nvim-telescope/telescope-media-files.nvim")
 
-    -- Telescope
-    use "nvim-telescope/telescope.nvim"
-    use 'nvim-telescope/telescope-media-files.nvim'
+		-- Color schemes
+		use({ "dracula/vim", as = "dracula" })
 
-    -- Color schemes
-    use { 'dracula/vim', as = 'dracula'}
+		-- Git
+		use("lewis6991/gitsigns.nvim")
 
-    -- Git
-    use "lewis6991/gitsigns.nvim"
+		-- Lines
+		use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" })
+		use({
+			"nvim-lualine/lualine.nvim",
+			requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		})
+	end
 
-    -- Lines
-    use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-
-  end
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
