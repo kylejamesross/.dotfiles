a0f848f03df7185b1576dc4a4f38eab94c933381
Kyle Ross <kylejamesross@gmail.com>
2022-08-22 18:27:00 -0600

    Add formatting on save

--- nvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -7,33 +7,46 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
-vim.env.PRETTIERD_DEFAULT_CONFIG = vim.fn.stdpath 'config' .. '/.prettierrc'
+vim.env.PRETTIERD_DEFAULT_CONFIG = vim.fn.stdpath("config") .. "/.prettierrc"
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-  debounce = 150,
-  autostart = true,
+	debounce = 150,
+	autostart = true,
 	debug = false,
 	sources = {
-      formatting.eslint_d,
-      diagnostics.eslint_d,
-      code_actions.eslint_d,
-      formatting.prettier.with {
-        prefer_local = 'node_modules/.bin',
-        filetypes = {
-          'typescriptreact',
-          'typescript',
-          'javascriptreact',
-          'javascript',
-          'svelte',
-          'json',
-          'jsonc',
-          'css',
-          'less',
-          'scss',
-          'html',
-          'yaml',
-        },
-      },
-      formatting.stylua,
+		formatting.eslint_d,
+		diagnostics.eslint_d,
+		code_actions.eslint_d,
+		formatting.prettier.with({
+			prefer_local = "node_modules/.bin",
+			filetypes = {
+				"typescriptreact",
+				"typescript",
+				"javascriptreact",
+				"javascript",
+				"svelte",
+				"json",
+				"jsonc",
+				"css",
+				"less",
+				"scss",
+				"html",
+				"yaml",
+			},
+		}),
+		formatting.stylua,
 	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
