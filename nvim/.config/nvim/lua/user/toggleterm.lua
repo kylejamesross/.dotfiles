a6158b6dc3a7ce47c64c714fb7128a1c9af3946e
Kyle Ross <kylejamesross@gmail.com>
2022-08-23 22:27:48 -0600

    Adjust tmux sessionizer to be better

--- nvim/.config/nvim/lua/user/toggleterm.lua
+++ nvim/.config/nvim/lua/user/toggleterm.lua
@@ -3,23 +3,22 @@ if not status_ok then
 	return
 end
 
-toggleterm.setup{
-  direction = 'float',
-  open_mapping = [[<C-\>]],
-  float_opts = {
-    border = "curved",
+toggleterm.setup({
+	direction = "float",
+	open_mapping = [[<C-\>]],
+	float_opts = {
+		border = "curved",
 		winblend = 0,
 		highlights = {
 			border = "curved",
 			background = "Normal",
 		},
-  }
-}
+	},
+})
 
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
 function _lazygit_toggle()
-  lazygit:toggle()
+	lazygit:toggle()
 end
-
