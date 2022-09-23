ceba9276d256a56aec0cef2f5cb5301484fe40bf
Kyle Ross <kylejamesross@gmail.com>
2022-09-23 15:41:38 -0600

    Install emoji fonts and change font for awesome

--- .config/.config/awesome/theme.lua
+++ .config/.config/awesome/theme.lua
@@ -11,7 +11,7 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font = "NotoSansMono Nerd Font Medium 12"
+theme.font = "mplus Nerd Font medium 10"
 theme.wibar_opacity = 0.9
 
 theme.bg_normal = "#282a36"
