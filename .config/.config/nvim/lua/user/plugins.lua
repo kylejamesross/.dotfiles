5a2a229959e3fc2cef9e1ab7193aa82754fe721e
Kyle Ross <kylejamesross@gmail.com>
2022-12-11 18:34:28 -0700

    Add scrollbar information plugin to nvim

--- .config/.config/nvim/lua/user/plugins.lua
+++ .config/.config/nvim/lua/user/plugins.lua
@@ -127,6 +127,9 @@ return packer.startup(function(use)
 			"nvim-lualine/lualine.nvim",
 			requires = { "kyazdani42/nvim-web-devicons", opt = true },
 		})
+
+		-- Scrolling
+		use("petertriho/nvim-scrollbar")
 	end
 
 	-- Automatically set up your configuration after cloning packer.nvim
