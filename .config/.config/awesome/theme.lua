a776136f961a88a0e5fb5d87b07e9aa60e1e715c
Kyle Ross <kylejamesross@gmail.com>
2023-01-07 20:49:02 -0700

    Use JetBrainsMono for awesome font

--- .config/.config/awesome/theme.lua
+++ .config/.config/awesome/theme.lua
@@ -11,7 +11,7 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font = "mplus Nerd Font medium 10"
+theme.font = "JetBrainsMono Nerd Font SemiBold 11"
 theme.wibar_opacity = 0.9
 
 theme.bg_normal = "#282a36"
