adfb336b470a622aa1ad98005f3630c0be968200
Kyle Ross <kylejamesross@gmail.com>
2023-06-20 08:39:40 -0600

    Add initial nix flake

---
+++ modules/nixos/hyprland/default.nix
@@ -0,0 +1,72 @@
+#
+#  Hyprland configuration
+#
+#  flake.nix
+#   ├─ ./hosts
+#   │   └─ ./<host>
+#   │       └─ default.nix
+#   └─ ./modules
+#       └─ ./desktop
+#           └─ ./hyprland
+#               └─ default.nix *
+#
+
+{ config, lib, pkgs, hyprland, ... }:
+let
+  exec = "exec Hyprland";
+in
+{
+
+  programs.waybar = {
+    enable = true;
+  };
+
+  environment = {
+    loginShellInit = ''
+      if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+        ${exec}
+      fi
+    '';                                   # Will automatically open Hyprland when logged into tty1
+
+    variables = {
+      #WLR_NO_HARDWARE_CURSORS="1";         # Possible variables needed in vm
+      #WLR_RENDERER_ALLOW_SOFTWARE="1";
+      XDG_CURRENT_DESKTOP="Hyprland";
+      XDG_SESSION_TYPE="wayland";
+      XDG_SESSION_DESKTOP="Hyprland";
+    };
+    sessionVariables = {
+      QT_QPA_PLATFORM = "wayland";
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      GDK_BACKEND = "wayland";
+      WLR_NO_HARDWARE_CURSORS = "1";
+      MOZ_ENABLE_WAYLAND = "1";
+    };
+    systemPackages = with pkgs; [
+      grim
+      mpvpaper
+      slurp
+      swappy
+      swaylock
+      wl-clipboard
+      wlr-randr
+    ];
+  };
+
+  security.pam.services.swaylock = {
+    text = ''
+     auth include login
+    '';
+  };
+
+  programs = {
+    hyprland = {
+      enable = true;
+    };
+  };
+
+  xdg.portal = {                                  # Required for flatpak with window managers and for file browsing
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,188 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+
+{ inputs, outputs, lib, config, pkgs, ... }: {
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/nixos):
+    # outputs.nixosModules.example
+
+    # Or modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.home-manager
+    ../modules/nixos/hyprland/default.nix
+  ];
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs outputs; };
+    users = {
+      kyle = import ../home-manager/home.nix;
+    };
+  };
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  nix = {
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    settings ={
+      experimental-features = "nix-command flakes";
+      auto-optimise-store = true;           # Optimise syslinks
+    };
+    gc = {                                  # Automatic garbage collection
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 2d";
+    };
+    package = pkgs.nixVersions.unstable;    # Enable nixFlakes on system
+    registry.nixpkgs.flake = inputs.nixpkgs;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+      keep-outputs          = true
+      keep-derivations      = true
+    '';
+  };
+
+  networking.hostName = "desktop";
+
+  boot.loader.systemd-boot.enable = true;
+  users.users = {
+    kyle = {
+      initialPassword = "password";
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+      ];
+      extraGroups = [ "wheel" ];
+    };
+  };
+
+  security.sudo.wheelNeedsPassword = false; # User does not need to give password when using sudo.
+
+  time.timeZone = "America/Edmonton";        # Time zone and internationalisation
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";                          # or us/azerty/etc
+  };
+
+  security.rtkit.enable = true;
+  security.polkit.enable = true;
+
+  fonts.fonts = with pkgs; [                # Fonts
+    carlito                                 # NixOS
+    vegur                                   # NixOS
+    source-code-pro
+    jetbrains-mono
+    font-awesome                            # Icons
+    corefonts                               # MS
+    (nerdfonts.override {                   # Nerdfont Icons override
+      fonts = [
+        "FiraCode"
+      ];
+    })
+  ];
+
+  environment = {
+    variables = {
+      TERMINAL = "kitty";
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+    };
+    systemPackages = with pkgs; [           # Default packages installed system-wide
+      #vim
+      git
+      waybar
+      killall
+      nano
+      pciutils
+      usbutils
+      wget
+    ];
+  };
+
+  services = {
+    printing = {                                # Printing and drivers for TS5300
+      enable = true;
+      #drivers = [ pkgs.cnijfilter2 ];          # There is the possibility cups will complain about missing cmdtocanonij3. I guess this is just an error that can be ignored for now. Also no longer need required since server uses ipp to share printer over network.
+    };
+    pipewire = {                            # Sound
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+      jack.enable = true;
+    };
+    openssh = {                             # SSH: secure shell (remote connection to shell of server)
+      passwordAuthentication = false;
+      permitRootLogin = "no";
+      enable = true;                        # local: $ ssh <user>@<ip>
+                                            # public:
+                                            #   - port forward 22 TCP to server
+                                            #   - in case you want to use the domain name insted of the ip:
+                                            #       - for me, via cloudflare, create an A record with name "ssh" to the correct ip without proxy
+                                            #   - connect via ssh <user>@<ip or ssh.domain>
+                                            # generating a key:
+                                            #   - $ ssh-keygen   |  ssh-copy-id <ip/domain>  |  ssh-add
+                                            #   - if ssh-add does not work: $ eval `ssh-agent -s`
+      allowSFTP = true;                     # SFTP: secure file transfer protocol (send file to server)
+                                            # connect: $ sftp <user>@<ip/domain>
+                                            #   or with file browser: sftp://<ip address>
+                                            # commands:
+                                            #   - lpwd & pwd = print (local) parent working directory
+                                            #   - put/get <filename> = send or receive file
+      extraConfig = ''
+        HostKeyAlgorithms +ssh-rsa
+      '';                                   # Temporary extra config so ssh will work in guacamole
+    };
+    flatpak.enable = true;                  # download flatpak file from website - sudo flatpak install <path> - reboot if not showing up
+                                            # sudo flatpak uninstall --delete-data <app-id> (> flatpak list --app) - flatpak uninstall --unused
+                                            # List:
+                                            # com.obsproject.Studio
+                                            # com.parsecgaming.parsec
+                                            # com.usebottles.bottles
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system = {                                # NixOS settings
+    autoUpgrade = {                         # Allow auto update (not useful in flakes)
+      enable = true;
+      channel = "https://nixos.org/channels/nixos-unstable";
+    };
+    stateVersion = "23.05";
+  };
+}
